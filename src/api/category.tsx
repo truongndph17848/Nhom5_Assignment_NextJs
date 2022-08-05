bb471ade4e99374c6c9d8556d095c9148ebe8de3
KhongBinhMinh <minhkbph17672@fpt.edu.vn>
2022-08-05 21:03:02 +0700

    update api-category

--- src/api/category.tsx
+++ src/api/category.tsx
@@ -3,12 +3,15 @@ import instance from "./instance";
 // export const getAll = () => {
 //     return instance.get('/products');
 // }
-export const addCategory = (category: any) => {
+export const add = (category: any) => {
     return instance.post("/categories", category);
 };
-export const removeCategory = (id: any) => {
+export const removeItem = (id: any) => {
     return instance.delete(`/categories/${id}`);
 };
-export const getCategory = (id: any) => {
+export const read = (id: any) => {
     return instance.get(`/categories/${id}`);
+};
+export const update = (category: any) => {
+    return instance.put(`/categories/${category.id}`, category);
 };
