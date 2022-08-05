0d7f1bc0ac98fa9ffb1a64bba1f9a4dbf67538e6
KhongBinhMinh <minhkbph17672@fpt.edu.vn>
2022-08-05 21:10:36 +0700

    update hook-category

--- src/hooks/use-category.tsx
+++ src/hooks/use-category.tsx
@@ -1,28 +1,31 @@
-import { addCategory, removeCategory, getCategory } from "@/api/category";
+import { add, removeItem, update } from "@/api/category";
 import useSWR from "swr";
-import { PublicConfiguration } from "swr/dist/types";
-
 const useCategory = (options?) => {
     const { data, error, mutate } = useSWR("/categories", { dedupingInterval: 2000, ...options });
-
-    // create
     const create = async (item) => {
-        const category = await addCategory(item);
-        mutate([...data, category]);
+        const product = await add(item);
+        mutate([...data, product]);
     };
-    // update
-    // delete
-    const remove = async (id) => {
-        await removeCategory(id);
-        const newCategories = data.filter((item) => item.id != id);
-        mutate(newCategories);
+    const remove = async (id: any) => {
+        const confirm = window.confirm('Bạn có muốn xóa không ?')
+        if (confirm) {
+            await removeItem(id);
+            const newProducts = data.filter((item) => item.id != id);
+            mutate(newProducts);
+        }
     };
+    const editCategory = async (product: any) => {
+        await update(product)
+        mutate(data.map((item: any) => (item.id === data.id ? product : item)))
+    }
     return {
         data,
         error,
         create,
         remove,
+        editCategory
     };
 };
 
 export default useCategory;
+
