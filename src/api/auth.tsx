b2842c5d24df1ea2cf547e69527160568879d3f3
Le Duc Dung <dungldc6ts42020@gmail.com>
2022-08-06 21:59:54 +0700

    fix: login

--- src/api/auth.tsx
+++ src/api/auth.tsx
@@ -3,6 +3,6 @@ import instance from "./instance";
 export const register = (user) => {
     return instance.post("/users",user);
 }
-export const login = (user) => {
-    return instance.get("/users",user);
+export const login = (user: { email: string; password: string }) => {
+    return instance.post("/signin",user);
 }
