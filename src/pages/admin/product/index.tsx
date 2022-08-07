70215470780dc587ea222f8ec1523b3f89461736
kienhi123 <kien123@gmail.com>
2022-08-07 22:33:54 +0700

    update : table-admin

--- src/pages/admin/product/index.tsx
+++ src/pages/admin/product/index.tsx
@@ -25,19 +25,19 @@ const listProduct = ({ products }: ProductsProps) => {
                 <thead className="">
                   <tr>
                     <th scope="col" className="text-base font-medium text-gray-900 px-6 py-4 text-left border">
-                      Id
+                      STT
                     </th>
-                    <th scope="col" className="text-base font-medium text-gray-900 px-6 py-4 text-left border">
+                    <th scope="col" className="text-base font-medium text-gray-900 px-3 py-4 text-left border">
                       Name
-                    </th>
-                    <th scope="col" className="text-base font-medium text-gray-900 px-6 py-4 text-left border">
-                      Price
                     </th>
                     <th scope="col" className=" text-base text-center  font-medium text-gray-900 px-6 py-4 text-left border ">
                       Image
                     </th>
-                    <th scope="col" className=" text-base text-center  font-medium text-gray-900 px-6 py-4 text-left border ">
-                      Phân loại
+                    <th scope="col" className="text-base font-medium text-gray-900 px-6 py-4 text-left border">
+                      PriceNew
+                    </th>
+                    <th scope="col" className="text-base font-medium text-gray-900 px-6 py-4 text-left border">
+                      PriceOld
                     </th>
                     <th scope="col" className="text-base font-medium text-gray-900 px-6 py-4 text-left border">
                       Trạng thái
@@ -50,24 +50,24 @@ const listProduct = ({ products }: ProductsProps) => {
                 <tbody>
                   {data.map((item: any, index: any) => (
                     <tr key={index} className="border-solid	">
-                      <td className=" px-6 py-4 whitespace-nowrap text-base font-medium text-gray-900 border" >{item.id}</td>
-                      <td className="text-base text-gray-900 font-light px-6 py-4 whitespace-nowrap border">
+                      <td className=" px-6 py-4 whitespace-nowrap text-base font-medium text-gray-900 border" >{index + 1}</td>
+                      <td className="text-base text-gray-900 font-light px-3 py-4 whitespace-nowrap border">
                         {item.name}
-                      </td>
-                      <td className="text-base text-gray-900 font-light px-6 py-4 whitespace-nowrap border">
-                        {item.price}
                       </td>
                       <td className="text-base text-gray-900 font-light px-6 py-4 whitespace-nowrap border ">
                         <img src={item.img} alt="" width="120px" />
                       </td>
                       <td className="text-base text-gray-900 font-light px-6 py-4 whitespace-nowrap border">
-                        {item.desc}
+                        {item.pricenew}
+                      </td>
+                      <td className="text-base text-gray-900 font-light px-6 py-4 whitespace-nowrap border">
+                        {item.priceold}
                       </td>
                       <td className="text-base text-gray-900 font-light px-6 py-4 whitespace-nowrap border">
                         {item.status}
                       </td>
                       <td className='border '>
-                        <button className='bg-red-300 px-2 py-2 text-center border' onClick={() => remove(item.id)}>Delete</button>
+                        <button className='bg-red-300 px-2 py-2 text-center border' onClick={() => remove(item._id)}>Delete</button>
                         <button className="bg-blue-300 px-2 py-2 text-center border "> <Link href={`product/${item.id}`}>Edit</Link></button>
 
 
@@ -83,52 +83,6 @@ const listProduct = ({ products }: ProductsProps) => {
         </div>
       </div>
 
-      {/* <table >
-        <thead>
-          <tr className='border'>
-            <th scope="col" className="text-base   font-medium text-gray-900 px-6 py-4 text-left">
-              id
-            </th>
-            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-              Name
-            </th>
-            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-              Price
-            </th>
-            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-              Handle
-            </th>
-            <th colSpan={2}>  <button> <Link href="product/create"> Thêm sản phẩm </Link> </button> </th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {data.map((item: any, index: any) => (
-            <tr key={index}>
-              <td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>{item.id}</td>
-              <td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'> <Link href={`/products/${item.id}`}>{item.name}</Link> </td>
-              <td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>***</td>
-              <td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>***</td>
-
-
-              <td><button onClick={() => remove(item.id)}>Delete</button></td>
-
-              <td></td>
-            </tr>
-          ))}
-
-        </tbody>
-      </table> */}
-
-
-
-      {/* {data.map((item: any) => (
-      <div key={item.id}>
-          <Link href={`/products/${item.id}`}>{item.name}</Link>
-
-      </div>
-  ))}
-<button onClick={() => create({ id: 14, name: "Product 14" })}>Create</button> */}
 
     </div>
   )
